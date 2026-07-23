fffd5c90d5c22fc536acfe20d15850c0b697a640
Sejal Solanki <ersejal.solanki@gmail.com>
2026-07-23 21:40:48 +0530

    Create pattern3.java

---
+++ PATTERN_PROGRAMS.java/pattern3.java
@@ -0,0 +1,26 @@
+/* this program prints the following pattern (for n=5)
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5 */
+
+import java.util. Scanner;
+public class  pat2
+{
+    public static void main(String[]args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter n:");
+        int n=sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++)
+          {
+            for(j=1;j<=i;j++)
+              {
+                System.out.print(j);
+              }
+            System.out.println();
+          }
+    }
+}
